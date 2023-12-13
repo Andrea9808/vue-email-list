1938006b77e77ca72708c6dc1a2a3779ce899c98
Andrea9808 <andrea.manigrasso98@gmail.com>
2023-12-13 14:49:58 +0100

    feat: generati li in pagina

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+const { createApp } = Vue;
+
+createApp({
+
+    data(){
+        
+        return{
+
+            mails: [],
+            
+        }
+    },
+
+    mounted(){
+
+        for(let i = 0; i < 10; i++){
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+            this.mails.push(response.data.response)
+            });
+    
+        }
+       
+    }
+
+}).mount('#app');
